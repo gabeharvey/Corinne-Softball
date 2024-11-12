d9d22ad28bdef47fbba15e5308278f229274909f
Gabe Harvey <harvey7@gmail.com>
2024-11-11 22:32:13 -0600

    Updated Logo Size on Back of Cards

--- src/components/PlayerCards.jsx
+++ src/components/PlayerCards.jsx
@@ -79,7 +79,7 @@ function Card({ frontImg, name, bio, funFact, cardNumber, flipSound }) {
           >
           {/* Team Logo */}
           <Box mb={2}>
-            <img src="texas-logo.png" alt="Team Logo" style={{ width: '50px', height: 'auto' }} />
+            <img src="texas-logo.png" alt="Team Logo" style={{ width: '100px', height: 'auto' }} />
           </Box>
 
           {/* Player Stats with Shadow */}
